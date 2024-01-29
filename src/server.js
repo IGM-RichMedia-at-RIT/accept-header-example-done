9b15afab2fd19f9139ab81b0dff5560fb044356e
Austin Willoughby <arwigm@rit.edu>
2024-01-29 13:52:23 -0500

    Package updates

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 const http = require('http'); // pull in the http server module
-const url = require('url'); // pull in the url module
 // pull in our response handler file
 const responseHandler = require('./responses.js');
 
@@ -18,7 +17,7 @@ const urlStruct = {
 const onRequest = (request, response) => {
   // parse the url using the url module
   // This will let us grab any section of the URL by name
-  const parsedUrl = url.parse(request.url);
+  const parsedUrl = new URL(request.url);
 
   // grab the 'accept' headers (comma delimited) and split them into an array
   const acceptedTypes = request.headers.accept.split(',');
