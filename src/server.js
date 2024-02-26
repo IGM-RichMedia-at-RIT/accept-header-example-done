e64281f1e550c4141fc2095e42b5f025102fd35f
Austin Willoughby <arwigm@rit.edu>
2024-02-26 17:10:27 -0500

    Testing new config

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
-const http = require('http'); // pull in the http server module
+const http = require('http'); // pull in the http server module 
+const url = require('url'); // pull in the url module
 // pull in our response handler file
 const responseHandler = require('./responses.js');
 
@@ -17,7 +18,7 @@ const urlStruct = {
 const onRequest = (request, response) => {
   // parse the url using the url module
   // This will let us grab any section of the URL by name
-  const parsedUrl = new URL(request.url);
+  const parsedUrl = url.parse(request.url);
 
   // grab the 'accept' headers (comma delimited) and split them into an array
   const acceptedTypes = request.headers.accept.split(',');
